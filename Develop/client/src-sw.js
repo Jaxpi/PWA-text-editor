2b680a37f2d191d9ebc72ad88df9053cb3e4ddf6
Jax I <jaxpi85@gmail.com>
2022-11-28 15:45:17 -0500

    src-sw file fixed

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -27,4 +27,16 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(({ request }) => request.destination === 'image',
+new CacheFirst({
+  cacheName: 'my-image-cache',
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+    new ExpirationPlugin({
+      maxEntries: 60,
+      maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+    }),
+  ],
+}));
